55704b9fb841aaa98454bd3bb4b6d6621a32695e
Rahul Katre <rahul.katre@a10networks.com>
2020-07-22 23:23:44 +0530

    L7 policy create and delete task

--- a10_octavia/controller/worker/tasks/l7policy_tasks.py
+++ a10_octavia/controller/worker/tasks/l7policy_tasks.py
@@ -18,127 +18,108 @@ from oslo_config import cfg
 from oslo_log import log as logging
 from taskflow import task
 
+from a10_octavia.controller.worker.tasks import utils
 from a10_octavia.controller.worker.tasks.decorators import axapi_client_decorator
 from a10_octavia.controller.worker.tasks.policy import PolicyUtil
-from a10_octavia.controller.worker.tasks import utils
 
 CONF = cfg.CONF
 LOG = logging.getLogger(__name__)
 
 
-class L7PolicyParent(object):
+class L7PolicyUtility(object):
 
-    def _create_l7policy(self):
-        pass
-
-    def _update_l7policy(self):
-        pass
-
-    def set(self, l7policy, listeners):
-        filename = l7policy.id
+    def _create_l7policy(self, l7policy):
+        file = l7policy.id
         p = PolicyUtil()
         script = p.createPolicy(l7policy)
         size = len(script.encode('utf-8'))
-        listener = listeners[0]
+        self.axapi_client.slb.aflex_policy.create(
+            file=file, script=script, size=size, action="import")
+
+    def _get_virtual_port(self, listener):
+        return self.axapi_client.slb.virtual_server.vport.get(
+            listener.load_balancer_id, listener.name,
+            listener.protocol, listener.protocol_port)
+
+    def _update_virtual_port(self, listener, **kwargs):
         c_pers, s_pers = utils.get_sess_pers_templates(listener.default_pool)
-        kargs = {}
-        get_listener = None
-        try:
-            self.axapi_client.slb.aflex_policy.create(
-                file=filename, script=script, size=size, action="import")
-            LOG.debug("l7policy created successfully: %s", l7policy.id)
-        except acos_errors:
-            pass
-        except Exception as e:
-            LOG.exception("Failed to create/update l7policy: %s", str(e))
-            raise
-
-        try:
-            get_listener = self.axapi_client.slb.virtual_server.vport.get(
-                listener.load_balancer_id, listener.name,
-                listener.protocol, listener.protocol_port)
-        except Exception as e:
-            LOG.exception("Failed to get listener for l7policy: %s", str(e))
-            raise
-
-        aflex_scripts = []
-        if 'aflex-scripts' in get_listener['port']:
-            aflex_scripts = get_listener['port']['aflex-scripts']
-            aflex_scripts.append({"aflex": filename})
-        else:
-            aflex_scripts = [{"aflex": filename}]
-            kargs["aflex-scripts"] = aflex_scripts
-
-        try:
-            self.axapi_client.slb.virtual_server.vport.update(
-                listener.load_balancer_id, listener.name,
-                listener.protocol, listener.protocol_port,
-                listener.default_pool_id, s_pers,
-                c_pers, 1, **kargs)
-            LOG.debug("Listener updated successfully: %s", listener.id)
-        except Exception as e:
-            LOG.exception("Failed to update listener for l7policy: %s", str(e))
-            raise
+        self.axapi_client.slb.virtual_server.vport.update(
+            listener.load_balancer_id, listener.name,
+            listener.protocol, listener.protocol_port,
+            listener.default_pool_id, s_pers,
+            c_pers, 1, **kwargs)
 
 
-class CreateL7Policy(L7PolicyParent, task.Task):
+class CreateL7Policy(L7PolicyUtility, task.Task):
     """Task to create a L7Policy"""
 
     @axapi_client_decorator
     def execute(self, l7policy, listeners, vthunder):
-        self.set(l7policy, listeners)
+        port_update_params = {}
+        listener = listeners[0]
+        try:
+            self._create_l7policy(l7policy)
+            LOG.debug("Successfully created L7 policy: %s", l7policy.id)
+        except (acos_errors.ACOSException, ConnectionError) as e:
+            LOG.exception("Failed to create L7 policy: %s", l7policy.id)
+            raise e
+        try:
+            listener_data = self._get_virtual_port(listener)
+        except (acos_errors.ACOSException, ConnectionError) as e:
+            LOG.exception("Failed to create L7 policy: %s", l7policy.id)
+            raise e
+        else:
+            try:
+                port_update_params['aflex-scripts'] = listener_data['port']['aflex-scripts']
+            except KeyError:
+                port_update_params['aflex-scripts'] = []
+            port_update_params.append({"aflex": l7policy.id})
+
+            try:
+                self._update_l7policy(listener, **port_update_params)
+                LOG.debug("Successfully associated L7 policy to port %s", l7policy.id. lis)
+            except (acos_errors.ACOSException, ConnectionError) as e:
+                LOG.exception("Failed to associate L7 policy %s to port %s", l7policy.id)
+                raise e
 
 
-class UpdateL7Policy(L7PolicyParent, task.Task):
+class UpdateL7Policy(L7PolicyUtility, task.Task):
     """Task to update L7Policy"""
 
     @axapi_client_decorator
     def execute(self, l7policy, listeners, vthunder, update_dict):
-        l7policy.__dict__.update(update_dict)
+        l7policy.update(update_dict)
         self.set(l7policy, listeners)
 
 
-class DeleteL7Policy(task.Task):
+class DeleteL7Policy(L7PolicyUtility, task.Task):
     """Task to delete L7Policy"""
 
     @axapi_client_decorator
     def execute(self, l7policy, vthunder):
+        update_params = {}
         listener = l7policy.listener
-        c_pers, s_pers = utils.get_sess_pers_templates(
-            listener.default_pool)
-        kargs = {}
-        get_listener = None
+        try:
+            listener_data = self._get_virtual_port(listener)
+        except (acos_errors.ACOSException, ConnectionError) as e:
+            raise e
 
         try:
-            get_listener = self.axapi_client.slb.virtual_server.vport.get(
-                listener.load_balancer_id, listener.name,
-                listener.protocol, listener.protocol_port)
-        except Exception as e:
-            LOG.warning("Failed to get listener for l7policy: %s", str(e))
-            raise
-
-        new_aflex_scripts = []
-        if 'aflex-scripts' in get_listener['port']:
-            aflex_scripts = get_listener['port']['aflex-scripts']
-            for aflex in aflex_scripts:
-                if aflex['aflex'] != l7policy.id:
-                    new_aflex_scripts.append(aflex)
-        kargs["aflex-scripts"] = new_aflex_scripts
-
-        try:
-            self.axapi_client.slb.virtual_server.vport.update(
-                listener.load_balancer_id, listener.name,
-                listener.protocol, listener.protocol_port,
-                listener.default_pool_id,
-                s_pers, c_pers, 1, **kargs)
-            LOG.debug(
-                "l7policy %s detached from port %s successfully.", l7policy.id, listener.id)
-        except Exception as e:
-            LOG.warning("Failed to detach l7policy: %s", str(e))
-            raise
+            aflex_scripts = listener_data['port']['aflex-scripts']
+        except KeyError:
+            pass
+        else:
+            update_params['aflex-scripts'] = [aflex for aflex in aflex_scripts if aflex['aflex'] != l7policy.id]
+            try:
+                self._update_virtual_port(l7policy, listener, **update_params)
+                LOG.debug("Successfully dissociated l7policy %s from port %s", l7policy.id, listener.id)
+            except (acos_errors.ACOSException, ConnectionError) as e:
+                LOG.warning("Failed to dissociate l7policy %s from port %s", l7policy.id, listener.id)
+                raise e
 
         try:
             self.axapi_client.slb.aflex_policy.delete(l7policy.id)
-            LOG.debug("l7policy deleted successfully: %s", l7policy.id)
-        except Exception as e:
-            LOG.warning("Failed to delete l7policy: %s", str(e))
+            LOG.debug("Successfully deleted l7policy: %s", l7policy.id)
+        except (acos_errors.ACOSException, ConnectionError) as e:
+            LOG.exception("Failed to delete l7policy: %s", l7policy.id)
+            raise e
